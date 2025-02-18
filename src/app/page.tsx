e6da0e4d996796a286303397a4944ee80a5a2872
JuniorFogassi <junior.fogassi@digihub.com.br>
2025-02-18 18:01:25 -0300

    Finishing button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+import { ArrowRight, Copy } from "lucide-react";
 import { Button } from "@/components/Button";
+import { IconButton } from "@/components/IconButton";
 
 export default function Home() {
   return (
     <main>
-      <div>Morgana, i love you!</div>
-      <Button />
+      <Button type="submit">
+        Send
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
     </main>
   );
 }
