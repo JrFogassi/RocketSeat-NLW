39f0218cf211b34636b04c94417aa0e2adf5eaa7
JuniorFogassi <junior.fogassi@digihub.com.br>
2025-02-19 10:20:04 -0300

    Setting biome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,3 @@
-import { ArrowRight, Copy, Mail } from "lucide-react";
-import { Button } from "@/components/Button";
-import { IconButton } from "@/components/IconButton";
-import { InputField, InputIcon, InputRoot } from "@/components/Input";
-
 export default function Home() {
-  return (
-    <main>
-      <Button type="submit">
-        Send
-        <ArrowRight />
-      </Button>
-
-      <IconButton>
-        <Copy />
-      </IconButton>
-
-      <div>
-        <InputRoot>
-          <InputIcon>
-            <Mail className="size-5"/>
-          </InputIcon>
-
-          <InputField />
-        </InputRoot>    
-      </div>
-    </main>
-  );
+  return <main></main>
 }
