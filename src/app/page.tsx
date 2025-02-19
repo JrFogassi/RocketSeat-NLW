0b39f6ce241ffc2f15ff2c73edb84a25cf395d58
JuniorFogassi <junior.fogassi@digihub.com.br>
2025-02-19 03:08:18 -0300

    Finishing Input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import { ArrowRight, Copy } from "lucide-react";
+import { ArrowRight, Copy, Mail } from "lucide-react";
 import { Button } from "@/components/Button";
 import { IconButton } from "@/components/IconButton";
+import { InputField, InputIcon, InputRoot } from "@/components/Input";
 
 export default function Home() {
   return (
@@ -13,6 +14,16 @@ export default function Home() {
       <IconButton>
         <Copy />
       </IconButton>
+
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail className="size-5"/>
+          </InputIcon>
+
+          <InputField />
+        </InputRoot>    
+      </div>
     </main>
   );
 }
